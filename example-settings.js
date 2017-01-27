3eefd661f01d5ec1c449b0893cce4a85327a805e
Mario Siegenthaler <mario.siegenthaler@linkyard.ch>
2017-01-27 12:57:32 +0100

    Move example setting to separate file.

---
+++ example-settings.js
@@ -0,0 +1,34 @@
+module.exports = {
+  url: 'https://jsd.test.linkyard.ch/',
+  title: 'linkyard Test instance',
+  public: false,
+  trialLicense: {
+    user: 'atlassian-trials@linkyard.ch',
+    password: 'secret',
+    organization: 'linkyard'
+  },
+  license: 'asdasf323q...',
+  admin: {
+    displayName: 'Administrator',
+    email: 'admin@domain.com',
+    user: 'admin',
+    password: 'secret'
+  },
+  db: {
+    type: 'PostgreSQL',
+    host: 'postgres',
+    port: '5432',
+    database: 'postgres',
+    user: 'postgres',
+    password: 'secret'
+  },
+  mail: {
+    from: 'admin@domain.com',
+    prefix: '[TEST]',
+    host: 'smtp-relay.gmail.com',
+    port: 465,
+    secure: true,
+    user: '',
+    password: ''
+  }
+};
--- index.js
+++ index.js
@@ -2,41 +2,6 @@ const spawn = require('child_process').spawn;
 const mkdirp = require('mkdirp');
 const createWriteStream = require('fs').createWriteStream;
 
-// const exampleSettings = {
-//   url: 'https://jsd.test.linkyard.ch/',
-//   title: 'linkyard Test instance',
-//   public: false,
-//   trialLicense: {
-//     user: 'atlassian-trials@linkyard.ch',
-//     password: 'secret',
-//     organization: 'linkyard'
-//   },
-//   license: 'asdasf323q...',
-//   admin: {
-//     displayName: 'Administrator',
-//     email: 'admin@domain.com',
-//     user: 'admin',
-//     password: 'secret'
-//   },
-//   db: {
-//     type: 'PostgreSQL',
-//     host: 'postgres',
-//     port: '5432',
-//     database: 'postgres',
-//     user: 'postgres',
-//     password: 'secret'
-//   },
-//   mail: {
-//     from: 'admin@domain.com',
-//     prefix: '[TEST]',
-//     host: 'smtp-relay.gmail.com',
-//     port: 465,
-//     secure: true,
-//     user: '',
-//     password: ''
-//   }
-// };
-
 function settingsFromEnv() {
   const settings = {};
   settings.url = process.env['JIRA_URL'];
